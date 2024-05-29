116bbb591102810c7031c88c1bf6ed3a730f536d
CandyCrussh <nikhilbharti199@gmail.com>
2024-05-29 15:08:20 +0530

    BankingApp removed

--- BankingApp/src/main/java/com/java/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.java.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.java.entity.Account;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-
-}
